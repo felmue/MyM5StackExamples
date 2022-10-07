8565db2269c198da11e5e55f0e36da48fcdff504
felmue <reg2000@gmx.net>
2022-10-07 15:55:41 +0200

    Create DeepSleepWakeFromTouch.ino

---
+++ M5Core2/DeepSleepWakeFromTouch.ino
@@ -0,0 +1,19 @@
+// Copyright (c) 2022 by GWENDESIGN. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <M5Core2.h>
+
+void setup()
+{
+  M5.begin();
+
+  esp_sleep_enable_ext0_wakeup((gpio_num_t)CST_INT, LOW);
+  Serial.println("before deep sleep");
+  Serial.flush();
+  esp_deep_sleep_start();
+  Serial.println("never reached");
+}
+
+void loop()
+{
+}
