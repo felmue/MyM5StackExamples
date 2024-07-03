fd37d011696a3bb43f051edd109d4ff34ea99356
felmue <reg2000@gmx.net>
2024-07-03 21:16:40 +0200

    Update ReadDualButtonViaPbHub.ino

--- M5AtomS3/ReadDualButtonViaPbHub/ReadDualButtonViaPbHub.ino
+++ M5AtomS3/ReadDualButtonViaPbHub/ReadDualButtonViaPbHub.ino
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 by GWENDESIGN. All rights reserved.
+// Copyright (c) 2024 by GWENDESIGN. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 // You need: M5AtomS3, ATOMIC ABC, PbHub unit v1.1, Dual Button unit
