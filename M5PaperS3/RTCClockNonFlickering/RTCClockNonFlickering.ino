b60f0f0e34f2be6831973be715c5697291aabb43
felmue <reg2000@gmx.net>
2025-04-17 10:45:06 +0200

    Fix daylight saving time

--- M5PaperS3/RTCClockNonFlickering/RTCClockNonFlickering.ino
+++ M5PaperS3/RTCClockNonFlickering/RTCClockNonFlickering.ino
@@ -2,32 +2,33 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 // This RTC clock example keeps the system mostly in shutdown mode and
-//  only wakes up every 58 seconds for a brief period of time during
+//  only wakes up every 56 seconds for a brief period of time during
 //  which the time and date are updated on the eInk display.
 //
 // When started initially or via power button a full eInk display refresh
 //  is executed to clear the display.
 // The current time and date are fetched via NTP and shown on the eInk display.
 // After waiting for the full minute the system is put into shutdown
-//  mode for about 58 seconds.
-// When the RTC timer expires (just befor the next minute change) the
+//  mode for about 56 seconds.
+// When the RTC timer expires (just before the next minute change) the
 //  system is powered on.
 // The eInk display is updated with the current time and date.
-// Then the system goes back into shutdown mode for about 58 seconds and
+// Then the system goes back into shutdown mode for about 56 seconds and
 //  the cycle begins anew.
 // Every hour a full eInk display refresh is executed to keep the ink
 //  display crisp.
 
 #include <epdiy.h>
 #include <M5Unified.h>
+#include <M5GFX.h>
 #include <WiFi.h>
 #include "time.h"
 
 const char* ssid             = "YOUR_SSID";
 const char* password         = "YOUR_PASSWORD";
 const char* ntpServer        = "pool.ntp.org";
-const long gmtOffset_sec     = 3600;
-const int daylightOffset_sec = 3600;
+// Time zones (https://remotemonitoringsystems.ca/time-zone-abbreviations.php)
+const char* TZ_INFO = "CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00";
 
 // every hour at minute 45 do a full ink display refresh
 #define FULL_REFRESH_MINUTE (45)
@@ -89,7 +90,10 @@ void getNTPTime()
 
   Serial.println("Connected");
 
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  configTime(0, 0, ntpServer);
+  setenv("TZ", TZ_INFO, 1);
+  tzset();
+
   printLocalTimeAndSetRTC();
 
   WiFi.disconnect(true);
@@ -101,10 +105,31 @@ void drawTimeAndDate(m5::rtc_time_t time, m5::rtc_date_t date)
   char buf[11];
 
   M5.Display.startWrite();
-  snprintf(buf, 6, "%02d:%02d", time.hours, time.minutes);
-  M5.Display.drawString(buf, 180, 20, &AsciiFont24x48);
-  snprintf(buf, 11, "%02d.%02d.%02d", date.date, date.month, date.year - 2000);
-  M5.Display.drawString(buf, 6, 280, &AsciiFont24x48);
+  M5.Display.setTextSize(4);
+  M5.Display.setFont(&FreeSansBold24pt7b);
+  M5.Display.setTextDatum(TC_DATUM);
+  snprintf(buf, 8, " %02d:%02d ", time.hours, time.minutes);
+  M5.Display.drawString(buf, 960/2, 50);
+  snprintf(buf, 11, " %02d.%02d.%02d ", date.date, date.month, date.year - 2000);
+  M5.Display.drawString(buf, 960/2, 260);
+  M5.Display.endWrite();
+}
+
+void drawBatteryVoltage()
+{
+  int16_t bat = M5.Power.getBatteryVoltage();
+  M5.Display.startWrite();
+  M5.Display.fillRect(20, 480, 280, 60, TFT_BLACK);
+  M5.Display.endWrite();
+  M5.Display.startWrite();
+  M5.Display.fillRect(20, 480, 280, 60, TFT_WHITE);
+  M5.Display.endWrite();
+
+  M5.Display.startWrite();
+  M5.Display.setTextSize(3);
+  M5.Display.setFont(&FreeSansBold12pt7b);
+  M5.Display.setTextDatum(TL_DATUM);
+  M5.Display.drawString(String(bat) + String("mV"), 20, 480);
   M5.Display.endWrite();
 }
 
@@ -135,7 +160,6 @@ void setup(void)
 
   M5.Power.setLed(127);
 
-  M5.Display.setTextSize(5.0);
   if (M5.Display.isEPD())
   {
     M5.Display.setEpdMode(epd_mode_t::epd_fastest);
@@ -172,6 +196,8 @@ void setup(void)
     M5.Rtc.getDate(&date);
   }
 
+  drawBatteryVoltage();
+
   // After every shutdown the eInk buffer is initialized anew.
   // But the eInk buffer doesn't match the current image on the
   //  ink display therefore the same time and date, as have been
@@ -198,12 +224,12 @@ void setup(void)
   if(time.minutes == FULL_REFRESH_MINUTE - 1)
   {
     // Allow extra time for full ink refresh
-    // Shutdown for 55 seconds only
-    M5.Power.timerSleep(55);
+    // Shutdown for 53 seconds only
+    M5.Power.timerSleep(53);
     return;
   }
-  // Shutdown for 58 seconds
-  M5.Power.timerSleep(58);
+  // Shutdown for 56 seconds
+  M5.Power.timerSleep(56);
 }
 
 void loop(void)
